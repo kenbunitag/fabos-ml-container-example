1fa4f23ea0bc4e9fbeb3c50d863fc3d339dd67fe
Simon Ottenhaus <simon.ottenhaus@kenbun.de>
2021-04-16 17:48:59 +0200

    added maskrcnn

--- app.py
+++ app.py
@@ -1,47 +1,23 @@
-import torch
-import torchvision
-import torchvision.models as models
-from PIL import Image
-from torchvision import transforms
-import numpy as np
-from omegaconf import OmegaConf
-
-from flask import Flask, flash, request, redirect, url_for
+import ml_models
+from flask import Flask, flash, request, redirect, url_for, send_file
 app = Flask(__name__)
 
-model = models.resnet18(pretrained=True)
-label_names = OmegaConf.load("labels.yaml")["labels"]
-print(f"loaded {len(label_names)} label names")
+resnet18 = ml_models.Resnet18Wrapper()
+mask_rcnn = ml_models.MaskRCNNWrapper()
 
-def do_inference(path):
-    input_image = Image.open(path)
-    print(input_image)
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
-    # move the input and model to GPU for speed if available
-    if torch.cuda.is_available():
-        input_batch = input_batch.to('cuda')
-        model.to('cuda')
+@app.route('/')
+def main():
+    return '''
+    <!doctype html>
+    <title>ML Inference demo</title>
+    <h1>ML Inference demo</h1>
+    <a href="/resnet18">ResNet18 Demo</a><br />
+    <a href="/maskrcnn">MaskRCNN Demo</a><br />
+    '''
 
-    with torch.no_grad():
-        output = model(input_batch)
-    # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-    #print(output[0]) 
-    # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-    probs = torch.nn.functional.softmax(output[0], dim=0)
-    top_probs = torch.topk(probs, k=5)
-    matching_labels = [label_names[i.item()] for i in top_probs.indices]
-    return(dict(values=top_probs.values.tolist(), indices=top_probs.indices.tolist(), names=matching_labels))
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/resnet18', methods=['GET', 'POST'])
+def resnet18_demo():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -53,12 +29,38 @@ def upload_file():
         print("saving ot tmp.jpg")
         file.save("tmp.jpg")
         print("do_inference tmp.jpg")
-        return do_inference("tmp.jpg")
+        return resnet18.do_inference("tmp.jpg")
         
     return '''
     <!doctype html>
-    <title>Upload jpg File</title>
-    <h1>Upload jpg File</h1>
+    <title>ResNet18 Demo</title>
+    <h1>ResNet18 Demo</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/maskrcnn', methods=['GET', 'POST'])
+def maskrcnn_demo():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        print("saving ot tmp.jpg")
+        file.save("tmp.jpg")
+        print("do_inference tmp.jpg")
+        mask_rcnn.instance_segmentation_api("tmp.jpg")
+        return send_file("mask_rcnn_result.png")
+        
+    return '''
+    <!doctype html>
+    <title>MaskRCNN Demo</title>
+    <h1>MaskRCNN Demo</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
@@ -67,7 +69,7 @@ def upload_file():
 
 @app.route('/banana', methods=['GET', 'POST'])
 def infer():
-    return do_inference("images/Banana-Single.jpg")
+    return resnet18.do_inference("images/Banana-Single.jpg")
 
 if __name__ == '__main__':
-    app.run(host='localhost', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
